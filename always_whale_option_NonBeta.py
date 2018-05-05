bf9101f03f9bccd52e183edd8a10b25947d01748
st4nza <32633587+st4nza@users.noreply.github.com>
2018-05-05 17:45:41 -0400

    Update always_whale_option_NonBeta.py

--- always_whale_option_NonBeta.py
+++ always_whale_option_NonBeta.py
@@ -1,13 +1,17 @@
+import json
+
 Location = r'C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire\preferences\STSPlayer'
 
 with open(Location) as f:
     lst2=f.read().splitlines()
 
-for x in range(len(lst2)):
-    if '"0"' in lst2[x]:
-        lst2[x]=lst2[x].replace('"0"','"1"')
+with open(Location) as f:
+    data = json.load(f)
 
-with open(Location, 'w') as filehandle:  
-    for listitem in lst2:
-        filehandle.write('%s\n' % listitem)
+for key, value in data.items():
+    if value == '0':
+        data[key] = '1'
+
+with open(Location, 'w') as outfile:
+    json.dump(data, outfile)
 
