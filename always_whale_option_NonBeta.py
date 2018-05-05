3c12f74fd2ed2573f3a6ab2f8aeab8fce30e69d3
st4nza <32633587+st4nza@users.noreply.github.com>
2018-05-05 16:12:05 -0400

    Update always_whale_option_NonBeta.py

--- always_whale_option_NonBeta.py
+++ always_whale_option_NonBeta.py
@@ -3,8 +3,9 @@ Location = r'C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire\preferen
 with open(Location) as f:
     lst2=f.read().splitlines()
 
-lst2[5]=lst2[5].replace("0","1")
-lst2[4]=lst2[4].replace("0","1")
+for x in range(len(lst2)):
+    if '"0"' in lst2[x]:
+        lst2[x]=lst2[x].replace('"0"','"1"')
 
 with open(Location, 'w') as filehandle:  
     for listitem in lst2:
