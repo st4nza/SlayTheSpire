7af16404268faf0e25f69beb090dabafd93ac34d
st4nza <32633587+st4nza@users.noreply.github.com>
2018-05-05 06:12:47 -0400

    Update always_whale_option_NonBeta.py

--- always_whale_option_NonBeta.py
+++ always_whale_option_NonBeta.py
@@ -1,34 +1,12 @@
-import pandas as pd
-import csv
-
 Location = r'C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire\preferences\STSPlayer'
-Location2 = r'C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire\preferences\test'
 
-df1 = pd.read_csv(Location,skiprows=1,sep=':', header=None)
-df1=df1.drop(df1.index[[6]])
-
-#if "0" in df1.iat[6,1]:
-#    df1.iat[6,1]=' "1"'
-    
-if "0" in df1.iloc[4,1]:
-    df1.iat[4,1]=' "1",'
-#    
-if "0" in df1.iloc[5,1]:
-    df1.iat[3,1]=' "1",'
-
-df1.to_csv(Location2, sep=':', index=False, header=False, quoting=csv.QUOTE_NONE)
-
-lst1=['{']
-    
-with open(Location2) as f:
+with open(Location) as f:
     lst2=f.read().splitlines()
 
-for x in range(len(lst2)):
-    lst1.append(lst2[x])
-
-lst1.append('}')
+lst2[5]=lst2[5].replace("0","1")
+lst2[4]=lst2[4].replace("0","1")
 
 with open(Location, 'w') as filehandle:  
-    for listitem in lst1:
+    for listitem in lst2:
         filehandle.write('%s\n' % listitem)
 
